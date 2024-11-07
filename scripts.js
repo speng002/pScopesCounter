545ac4173d6907dcfa5dbce75f178b1c54782764
Speng002 <Speng20@yahoo.com>
2024-11-06 23:50:25 -0800

    Made the Counter actually work

--- scripts.js
+++ scripts.js
@@ -1,13 +1,58 @@
-const countBtn = document.getElementById(count-Btn)
-// i need to link 1 more variable to javascript
+const countBtn = document.getElementById("count-Btn")
 
-countBtn.addEventListener("click", goCounting)
+countBtn.addEventListener("click", ()=>{
 
-function goCounting(){
-    countBtn = "";
 
-    function printHelloWorld(){
-        // idk if I use button or count-Btn
-        countBtn.innerHTML = "hello World"
+
+// function definition of code  
+
+    // calling createCounter every time - This keeps running createCounter() everytime I press the button
+    //  createCounter.increment;
+    
+
+    // reference to a function counter only runs 1 time 
+    // 1st Ref createCounter (You need to actually have it) (Line 44)
+    // 2nd Have the createCounter in my hands
+    // 3rd do the action
+        counter.increment();
+})
+
+
+function createCounter(){
+    let count = 0;
+
+    //Defining the different functions 
+    function increment(){
+    // both textContent & inner HTML works
+        count++;
+        // console.log is firing here
+        // console.log(`Count increased to ${count}`);
+        countBtn.innerHTML = `+ ${count}`;
     }
+    
+    function getCount(){
+        return count;
+    }
+//Actually use the functions
+// {} needed because can't return more than 1 thing
+    return {increment, getCount};
+
+
+    // This works
+    // countBtn.innerHTML = `+ ${getCount}`;
+// This works by itself
+//     countBtn.innerHTML = "hello hello"
+
+// if it is outside**
+// countBtn.innerHTML = `+ ${count}`;
 }
+
+const counter = createCounter();
+console.log(counter);
+
+
+// counter.getCount() = the number of clicks
+
+
+
+
