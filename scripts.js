3d4d5f5b3f8af398c5d8496d80ab37cc42858c08
Speng002 <Speng20@yahoo.com>
2024-11-06 21:03:36 -0800

    first commit

---
+++ scripts.js
@@ -0,0 +1,13 @@
+const countBtn = document.getElementById(count-Btn)
+// i need to link 1 more variable to javascript
+
+countBtn.addEventListener("click", goCounting)
+
+function goCounting(){
+    countBtn = "";
+
+    function printHelloWorld(){
+        // idk if I use button or count-Btn
+        countBtn.innerHTML = "hello World"
+    }
+}
